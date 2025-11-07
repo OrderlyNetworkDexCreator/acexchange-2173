70cc2c8cc06007d045bffab2480c5f0c6af5ebc7
Mario Reder <mario.reder@pm.me>
2025-11-07 13:35:49 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "ACExchange",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://ac.exchange",
+  "VITE_SEO_SITE_URL": "https://dex.orderly.network/acexchange-2173/",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
